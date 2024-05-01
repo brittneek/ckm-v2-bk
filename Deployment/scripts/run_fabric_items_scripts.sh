dd4f189e9dc96d62d03a953b18b96e25d9cba89e
bkeller108 <bkeller108@yahoo.com>
2024-05-01 10:47:36 -0500

    Update run_fabric_items_scripts.sh

--- Deployment/scripts/run_fabric_items_scripts.sh
+++ Deployment/scripts/run_fabric_items_scripts.sh
@@ -13,9 +13,6 @@ echo "Download Started"
 # Download the create_fabric python files
 curl --output "create_fabric_items.py" ${baseUrl}"Deployment/scripts/fabric_scripts/create_fabric_items.py"
 
-curl --output "create_articles_index.ipynb" ${baseUrl}"Deployment/scripts/fabric_scripts/create_articles_index.ipynb"
-curl --output "create_grants_index.ipynb" ${baseUrl}"Deployment/scripts/fabric_scripts/create_grants_index.ipynb"
-curl --output "create_drafts_index.ipynb" ${baseUrl}"Deployment/scripts/fabric_scripts/create_drafts_index.ipynb"
 
 # Download the requirement file
 curl --output "$requirementFile" "$requirementFileUrl"
@@ -26,9 +23,6 @@ echo "Download completed"
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_fabric_items.py"
 sed -i "s/workspaceId_to-be-replaced/${fabricWorkspaceId}/g" "create_fabric_items.py"
 
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_articles_index.ipynb"
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_grants_index.ipynb"
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_drafts_index.ipynb"
 
 pip install -r requirements.txt
 
