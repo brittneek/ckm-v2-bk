f462012d2ead5c1342bd19cf2bac1ebb65ba4942
Nalini Chandhi <nalini.chandhi@microsoft.com>
2024-05-13 18:27:43 -0700

    script changes

--- Deployment/scripts/fabric_scripts/create_fabric_items.py
+++ Deployment/scripts/fabric_scripts/create_fabric_items.py
@@ -4,6 +4,7 @@ import json
 import requests
 import pandas as pd
 import os
+from glob import iglob
 
 # credential = DefaultAzureCredential()
 from azure.identity import AzureCliCredential
@@ -39,7 +40,6 @@ if create_workspace == True:
   # print(ws_details['id'])
   workspaceId = ws_details['id']
 
-
 fabric_base_url = f"https://api.fabric.microsoft.com/v1/workspaces/{workspaceId}/"
 fabric_items_url = f"https://api.fabric.microsoft.com/v1/workspaces/{workspaceId}/items/"
 
@@ -49,15 +49,12 @@ fabric_create_workspace_url = f"https://api.fabric.microsoft.com/v1/workspaces"
 ws_res = requests.get(fabric_base_url, headers=fabric_headers)
 workspace_name = ws_res.json()['displayName']
 
-
 #create lakehouse
 lakehouse_data = {
   "displayName": lakehouse_name,
   "type": "Lakehouse"
 }
 lakehouse_res = requests.post(fabric_items_url, headers=fabric_headers, json=lakehouse_data)
-# lakehouse_res.json()
-
 
 # copy local files to lakehouse
 from azure.storage.filedatalake import (
@@ -68,7 +65,7 @@ from azure.storage.filedatalake import (
 
 account_name = "onelake" #always onelake
 data_path = f"{lakehouse_name}.Lakehouse/Files"
-folder_path = "data"
+folder_path = "/"
 
 account_url = f"https://{account_name}.dfs.fabric.microsoft.com"
 service_client = DataLakeServiceClient(account_url, credential=credential)
@@ -78,13 +75,9 @@ file_system_client = service_client.get_file_system_client(workspace_name)
 
 directory_client = file_system_client.get_directory_client(f"{data_path}/{folder_path}")
 
-local_data_path = '../../data'
-file_names = []
-for (dirpath, dirnames, filenames) in os.walk(local_data_path):
-    file_names.extend(filenames)
-    break
-
+local_path = 'data/**/*'
+file_names = [f for f in iglob(local_path, recursive=True) if os.path.isfile(f)]
 for file_name in file_names:
-    file_client = directory_client.get_file_client(file_name)
-    with open(file=os.path.join(local_data_path, file_name), mode="rb") as data:
-        file_client.upload_data(data, overwrite=True)
+  file_client = directory_client.get_file_client(file_name)
+  with open(file=file_name, mode="rb") as data:
+    file_client.upload_data(data, overwrite=True)
